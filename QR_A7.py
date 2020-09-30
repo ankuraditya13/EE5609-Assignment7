e94bf4419d7a349a2b56dd9f06d16368d880823a
ankuraditya13 <70592234+ankuraditya13@users.noreply.github.com>
2020-09-30 20:02:30 +0530

    Add files via upload

---
+++ QR_A7.py
@@ -0,0 +1,8 @@
+import numpy as np
+A=np.array([[2.,-6.],[1.,-2.]])
+print("Given matrix A is: \n",A)
+Q,R=np.linalg.qr(A)
+print("\nOrthogonal Matrix Q is :\n",Q)
+print("\nQ^TQ:\n",Q.T@Q)
+print("\nUpper Triangular Matrix R is: \n",R)
+print("\nA=QR=\n",Q@R)
